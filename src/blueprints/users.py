bbe86eb1db4e6d42a1eddeb344adb9fa5a18deea
diego-carvalho <diego.a.carvalho@hotmail.com>
2017-09-13 23:14:37 -0300

    crud users finished, missing update your data table

--- src/blueprints/users.py
+++ src/blueprints/users.py
@@ -56,107 +56,36 @@ def post():
     return print_json(res)
 
 @users_blueprint.route("/users/<int:id>", methods=['PUT'])
-def edit(id=None):
-    return "Edit User " + str(id)
+def put(id):
+    name = request.form.get('name')
 
-"""
-product_view = ProductView.as_view('product_view')
-app.add_url_rule(
-    '/product/', view_func=product_view, methods=['GET', 'POST']
-)
-app.add_url_rule(
-    '/product/<int:id>', view_func=product_view, methods=['GET', 'PUT', 'DELETE']
-)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(
+            "UPDATE users SET name='%s' WHERE id=%d" % (name, id))
+        res = {id: {
+            'name': name
+        }}
+        conn.commit()
+    except:
+        res = {'response': 'Error in change user values with id = %d!' % id}
+    cursor.close()
+    
+    return print_json(res)
 
-
-import sys
-
-from flask import request, jsonify, Blueprint, abort
-from my_app import mysql
-from flask.views import MethodView
-
-# print(products, file=sys.stderr)
-
-class ProductView(MethodView):
-    def get(self, id=None):
-        if not id:
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM product")
-            products = cursor.fetchall()
-            cursor.close()
-
-            res = {}
-            
-            if len(products) > 0:
-                for product in products:
-                    res[product[0]] = {
-                        'name': product[1],
-                        'price': product[2]
-                    }
-            
-        else:
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM product WHERE id = %d" % id)
-            product = cursor.fetchone()
-            cursor.close()
-
-            if not product:
-                abort(404)
-            res = {
-                'name': product[1],
-                'price': product[2]
-            }
-            print(res, file=sys.stderr)
-        return jsonify(res)
-
-    def post(self):
-        name = request.form.get('name')
-        price = request.form.get('price')
-
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        try:
-            cursor.execute("INSERT INTO product(name, price) VALUES ('%s', %s)" % (name, price))
-            res = {cursor.lastrowid: {
-                'name': name,
-                'price': price,
-            }}
-            conn.commit()
-        except:
-            res = {'response': 'Error in create user!'}
+@users_blueprint.route("/users/<int:id>", methods=['DELETE'])
+def delete(id):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    try:
+        user = get(id)
+        cursor.execute("DELETE FROM users WHERE id=%d" % id)
+        # res = {'response': 'Object is been remove with id = %d!' % id}
+        conn.commit()
         cursor.close()
-
-        return jsonify(res)
-
-    def put(self, id):
-        name = request.form.get('name')
-        price = request.form.get('price')
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        # print("UPDATE product SET name='%s', price=%s WHERE id=%d" % (name, price, id), file=sys.stderr)
-        try:
-            cursor.execute(
-                "UPDATE product SET name='%s', price=%s WHERE id=%d" % (name, price, id))
-            res = {id: {
-                'name': name,
-                'price': price,
-            }}
-            conn.commit()
-        except:
-            res = {'response': 'Error in change user values with id = %d!' % id}
+        return user
+    except:
+        res = {'response': 'Error in change user values with id = %d!' % id}
         cursor.close()
-        return jsonify(res)
- 
-    def delete(self, id):
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        try:
-            cursor.execute("DELETE FROM product WHERE id=%d" % id)
-            res = {'response': 'Object is been remove with id = %d!' % id}
-            conn.commit()
-        except:
-            res = {'response': 'Error in change user values with id = %d!' % id}
-        cursor.close()
-        return jsonify(res)"""
+        return print_json(res)
